19bf749594058001863be4c4802c591f0319af1b
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-03-27 08:50:29 +0100

    increase sleep

--- docker/flask/start_flask.sh
+++ docker/flask/start_flask.sh
@@ -3,7 +3,7 @@
 # echo "Starting script in the background"
 # python your_script.py &
 echo "Waiting for 3 seconds before starting the Flask app..."
-sleep 3
+sleep 5
 
 # export FLASK_APP=main.py
 flask run --host=0.0.0.0 --port=8081

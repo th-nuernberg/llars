a8d1a48351541127102eccf9a47ee36edaa296c0
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-03-27 10:51:30 +0100

    add routes, and JWTManager

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from db.db import configure_database
+from routes import configure_routes
+from flask_jwt_extended import JWTManager
 # Importiere die Modelle, damit sie von SQLAlchemy erkannt werden
 
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'super-geheimer-key'  # Ändere dies zu einem sicheren Schlüssel
 
+# Initialisiere JWTManager mit der Flask-App
+jwt = JWTManager(app)
 # Initialisiere die Datenbank mit der Flask-App
 configure_database(app)
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+configure_routes(app)
 
 if __name__ == '__main__':
     app.run(debug=True)

ac7ab869b90074b0c6f9a24860f246eb9a0f0ec3
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-03-28 09:08:14 +0100

    enable CORS

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from flask import Flask
 from db.db import configure_database
 from routes import configure_routes
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 # Importiere die Modelle, damit sie von SQLAlchemy erkannt werden
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'super-geheimer-key'  # Ändere dies zu einem sicheren Schlüssel
-
+CORS(app)
 # Initialisiere JWTManager mit der Flask-App
 jwt = JWTManager(app)
 # Initialisiere die Datenbank mit der Flask-App

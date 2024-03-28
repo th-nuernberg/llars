9ecd76720047cb42b3e912a8803e42eab700e2b4
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-03-28 10:53:17 +0100

    add router.js

---
+++ llars-frontend/src/router/router.js
@@ -0,0 +1,17 @@
+import {createRouter, createWebHistory} from "vue-router";
+import AnimalCollection from "@/components/AnimalCollection.vue";
+import FoodItems from "@/components/FoodItems.vue";
+import Login from "@/components/Login.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/animals', component: AnimalCollection },
+        { path: '/food', component: FoodItems },
+        { path: '/login', component: Login},
+        //{ path: '/', redirect: '/animals'}
+        //{ path: '/', redirect: '/animals',component: Login },
+    ]
+});
+
+export default router;

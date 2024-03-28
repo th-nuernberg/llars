d56e0237c28998cd59de241c0c7d3a15a5b69fc1
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-03-28 10:55:14 +0100

    update

--- llars-frontend/src/components/TheWelcome.vue
+++ llars-frontend/src/components/TheWelcome.vue
@@ -5,6 +5,7 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import { BASE_URL } from "@/config.js";
 </script>
 
 <template>
@@ -16,7 +17,7 @@ import SupportIcon from './icons/IconSupport.vue'
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    provides you with all information you need to get started. BASE_URL: {{ BASE_URL }}
   </WelcomeItem>
 
   <WelcomeItem>

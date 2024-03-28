6228b49c9d09915613a59acf15838d99b002cad3
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-03-28 10:53:50 +0100

    add router

--- llars-frontend/src/main.js
+++ llars-frontend/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import router from './router/router.js';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router);
+//app.component('food-items', FoodItems);
+//app.component('animal-collection', AnimalCollection);
+
+app.mount('#app')

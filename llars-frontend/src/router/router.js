a7e3bb017820423fc5e4d2493b42b57d568f00fa
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-04-01 14:56:47 +0200

    add 404 not found

--- llars-frontend/src/router/router.js
+++ llars-frontend/src/router/router.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import AnimalCollection from "@/components/AnimalCollection.vue";
 import FoodItems from "@/components/FoodItems.vue";
 import Login from "@/components/Login.vue";
+import NotFound from "@/components/NotFound.vue";
 
 const routes = [
     { path: '/animals', component: AnimalCollection, meta: { requiresAuth: true } },
     { path: '/food', component: FoodItems, meta: { requiresAuth: true } },
     { path: '/login', component: Login, meta: { requiresAuth: false } },
-    { path: '/', redirect: '/login' }
+    { path: '/', redirect: '/login' },
+     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound } // 404 Route
 ];
 
 const router = createRouter({

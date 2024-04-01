29224d9b9fb5d97baee74cea19ea42a44a228d5f
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-04-01 20:44:14 +0200

    add routes to ranker and rater

--- llars-frontend/src/router.js
+++ llars-frontend/src/router.js
@@ -2,10 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/components/Login.vue";
 import Home from "@/components/Home.vue";
 import NotFound from "@/components/NotFound.vue";
+import Ranker from "@/components/Ranker/Ranker.vue";
+import Rater from "@/components/Rater/Rater.vue";
 
 const routes = [
     { path: '/Home', component: Home, meta: { requiresAuth: true } },
+    { path: '/Ranker', component: Ranker, meta: { requiresAuth: true } },
+    { path: '/Rater', component: Rater, meta: { requiresAuth: true } },
     { path: '/login', component: Login, meta: { requiresAuth: false } },
+
     { path: '/', redirect: '/login' },
    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound } // 404 Route
 ];

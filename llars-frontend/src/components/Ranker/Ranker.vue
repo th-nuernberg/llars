2246d864d8c538fefe409da52a45c84dd5177b80
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-04-02 13:31:19 +0200

    beautify cards

--- llars-frontend/src/components/Ranker/Ranker.vue
+++ llars-frontend/src/components/Ranker/Ranker.vue
@@ -16,13 +16,16 @@
           >
             Default Kategorie
           </v-chip>
-          <v-card-title>{{ emailThread.subject }}</v-card-title>
-          <v-card-text>Chat ID: {{ emailThread.chat_id }}</v-card-text>
+          <div class="card-content">
+            <v-card-title>{{ emailThread.subject }}</v-card-title>
+            <v-card-text class="chat-id">{{ 'Chat ID: ' + emailThread.chat_id }}</v-card-text>
+          </div>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
 
 <script setup>
 import {ref, onMounted} from 'vue';
@@ -51,6 +54,9 @@ function navigateToCase(threadId) {
   position: relative;
   transition: box-shadow 0.3s ease-in-out, transform 0.1s ease-in-out;
   cursor: pointer;
+  height: 200px; /* Setzen Sie eine feste Höhe oder min-height für eine Mindesthöhe */
+  display: flex;
+  flex-direction: column; /* Organisieren Sie den Inhalt der Karte in einer Spalte */
 }
 
 .case-card:hover {
@@ -60,8 +66,35 @@ function navigateToCase(threadId) {
 
 .category-chip {
   position: absolute;
-  top: 4px;
-  right: 4px;
+  top: 8px; /* Vergrößern Sie den oberen Abstand, um Überlappungen zu vermeiden */
+  right: 8px; /* Fügen Sie etwas horizontalen Abstand hinzu */
   border-radius: 12px 5px 12px 5px;
+  z-index: 1; /* Stellen Sie sicher, dass der Chip über dem Titel liegt */
+}
+
+.card-content {
+  padding-top: 36px; /* Fügen Sie zusätzlichen Platz oben hinzu, damit der Titel nicht überdeckt wird */
+  padding-right: 36px; /* Fügen Sie auch rechts Platz hinzu, um Überlappungen mit dem Chip zu vermeiden */
+
+  flex-grow: 1; /* Lassen Sie den Inhalt den verfügbaren Platz ausfüllen */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; /* Verteilen Sie den Inhalt gleichmäßig in der Karte */
+}
+
+.v-card-title {
+  font-size: 1rem; /* Passen Sie die Schriftgröße nach Bedarf an */
+  white-space: normal; /* Erlauben Sie den Umbruch */
+  overflow: hidden; /* Verstecken Sie überlaufenden Text */
+  text-overflow: ellipsis; /* Zeigen Sie "..." am Ende des überlaufenden Textes an */
+  display: -webkit-box;
+  -webkit-line-clamp: 2; /* Begrenzen Sie auf 2 Zeilen */
+  -webkit-box-orient: vertical;
+}
+.v-card-text {
+  margin-top: auto; /* Schiebt den Text nach unten, weg vom Titel */
+}
+.chat-id {
+  align-self: auto; /* Positioniert die Chat-ID am Ende der Karte */
 }
 </style>

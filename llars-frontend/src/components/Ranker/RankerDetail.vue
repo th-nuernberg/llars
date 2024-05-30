dee1013d861fc0692e6138edfe99d3ff93462f5a
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-05-30 10:36:37 +0200

    clean up code

--- llars-frontend/src/components/Ranker/RankerDetail.vue
+++ llars-frontend/src/components/Ranker/RankerDetail.vue
@@ -3,38 +3,35 @@
     <v-row>
       <v-col cols="12" md="6">
         <h2>Features</h2>
-        <div class="email-thread">
-          <v-expansion-panels>
-            <v-expansion-panel v-for="feature in groupedFeatures" :key="feature.type">
-              <v-expansion-panel-title>
-                <div>{{ translateFeatureType(feature.type) }}</div>
-              </v-expansion-panel-title>
-              <v-expansion-panel-text>
-                <transition-group name="fade" tag="div">
-                  <draggable
-                    v-model="feature.details"
-                    group="featureGroup"
-                    item-key="feature_id"
-                    @start="handleDragStart"
-                    @end="handleDragEnd"
-                    v-bind="dragOptions"
-                    ghost-class="ghost"
-                    fallback-class="fallbackStyleClass"
-                    :force-fallback="true"
-                  >
-                    <template #item="{ element }">
-                      <div :key="element.feature_id" class="draggable-item no-select">
-                        <p><strong>Modell:</strong> {{ element.model_name }}</p>
-                        <p>{{ element.value }}</p>
-                      </div>
-                    </template>
-                  </draggable>
-                </transition-group>
-              </v-expansion-panel-text>
-            </v-expansion-panel>
-          </v-expansion-panels>
-          <v-spacer></v-spacer>
-        </div>
+        <v-expansion-panels>
+          <v-expansion-panel v-for="feature in groupedFeatures" :key="feature.type">
+            <v-expansion-panel-title>
+              <div>{{ translateFeatureType(feature.type) }}</div>
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <transition-group name="fade" tag="div">
+                <draggable
+                  v-model="feature.details"
+                  group="featureGroup"
+                  item-key="feature_id"
+                  @start="handleDragStart"
+                  @end="handleDragEnd"
+                  v-bind="dragOptions"
+                  ghost-class="ghost"
+                  fallback-class="fallbackStyleClass"
+                  :force-fallback="true"
+                >
+                  <template #item="{ element }">
+                    <div :key="element.feature_id" class="draggable-item no-select">
+                      <p><strong>Modell:</strong> {{ element.model_name }}</p>
+                      <p>{{ element.value }}</p>
+                    </div>
+                  </template>
+                </draggable>
+              </transition-group>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </v-col>
 
       <v-col cols="12" md="6">
@@ -61,8 +58,7 @@
         </div>
       </v-col>
     </v-row>
-    <v-spacer></v-spacer> <!-- Dies schiebt die Buttons nach unten -->
-
+    <v-spacer></v-spacer>
     <v-container fluid>
       <v-col cols="12" class="button-class">
         <v-btn @click="saveFeaturesServerSide">Speichern</v-btn>
@@ -94,35 +90,6 @@ const dragOptions = ref({
   ghostClass: 'ghost',
 });
 
-const drag = ref(false);
-
-async function fetchEmailThreads(threadId) {
-  try {
-    const response = await axios.get(`http://localhost:8081/api/email_threads/${threadId}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching email threads:', error);
-    return null; // Rückgabe von null bei einem Fehler
-  }
-}
-
-async function fetchServerRanking(threadId) {
-  try {
-    const api_key = localStorage.getItem('api_key');
-    console.log('API key:', api_key)
-    const response = await axios.get(`http://localhost:8081/api/email_threads/${threadId}/current_ranking`, {
-      headers: {
-        'Authorization': api_key
-      }
-    });
-    console.log('Server ranking:', response.data)
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching server ranking:', error);
-    return null; // Rückgabe von null bei einem Fehler
-  }
-}
-
 onMounted(async () => {
   const threadData = await fetchEmailThreads(route.params.id);
   if (!threadData) return;
@@ -142,16 +109,12 @@ onMounted(async () => {
       model_name: f.model_name,
       value: f.value,
       feature_id: f.feature_id,
-      position: index // include position
+      position: index
     });
   });
 
   groupedFeatures.value = Array.from(featureMap.values());
-
-  // Set the localStorage key based on thread ID
   localStorageKey.value = `featureOrder_${route.params.id}`;
-
-  // Load the saved order from LocalStorage
   await loadFeatureOrder();
 
   let lastSender = '';
@@ -164,6 +127,31 @@ onMounted(async () => {
     senderColors.value[message.sender] = currentColor;
   });
 });
+
+async function fetchEmailThreads(threadId) {
+  try {
+    const response = await axios.get(`http://localhost:8081/api/email_threads/${threadId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching email threads:', error);
+    return null;
+  }
+}
+
+async function fetchServerRanking(threadId) {
+  try {
+    const api_key = localStorage.getItem('api_key');
+    const response = await axios.get(`http://localhost:8081/api/email_threads/${threadId}/current_ranking`, {
+      headers: {
+        'Authorization': api_key
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching server ranking:', error);
+    return null;
+  }
+}
 
 async function loadFeatureOrder() {
   const savedOrder = localStorage.getItem(localStorageKey.value);
@@ -180,8 +168,6 @@ async function loadFeatureOrder() {
 
 function applyFeatureOrder(orderedFeatures) {
   const featureMap = new Map();
-
-  // Create a map with empty arrays for each feature type
   orderedFeatures.forEach(f => {
     featureMap.set(f.type, {
       type: f.type,
@@ -189,7 +175,6 @@ function applyFeatureOrder(orderedFeatures) {
     });
   });
 
-  // Populate the arrays with saved details in the correct positions
   orderedFeatures.forEach(f => {
     f.details.forEach(detail => {
       featureMap.get(f.type).details[detail.position] = detail;
@@ -214,27 +199,9 @@ function translateFeatureType(type) {
 }
 
 function formatTimestamp(timestamp) {
-  const options = {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'};
+  const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
   const date = new Date(timestamp);
-  const formattedDate = date.toLocaleDateString('de-DE', options).replace(',', ' um') + ' Uhr';
-  return formattedDate;
-}
-
-const colors = ["#E8F5E9", "#C8E6C9", "#F1F8E9", "#DCEDC8", "#a7ffeb", "#cbf0f8", "#aecbfa", "#d7aefb", "#fdcfe8", "#e6c9a8"];
-
-function getItemStyle(index) {
-  const backgroundColor = getColor(index);
-  return {
-    padding: '10px',
-    borderRadius: '10px',
-    margin: '5px 0',
-    backgroundColor: `rgba(${parseInt(backgroundColor.slice(1, 3), 16)}, ${parseInt(backgroundColor.slice(3, 5), 16)}, ${parseInt(backgroundColor.slice(5, 7), 16)}, 0.8)`,
-    color: '#000'
-  };
-}
-
-function getColor(index) {
-  return colors[index % colors.length];
+  return date.toLocaleDateString('de-DE', options).replace(',', ' um') + ' Uhr';
 }
 
 function handleDragStart() {
@@ -253,7 +220,7 @@ function saveFeatureOrderToLocalStorage() {
       model_name: detail.model_name,
       value: detail.value,
       feature_id: detail.feature_id,
-      position: index // save the current position
+      position: index
     }))
   }));
   localStorage.setItem(localStorageKey.value, JSON.stringify(orderedFeatures));
@@ -269,7 +236,7 @@ function navigateToPreviousCase() {
 
 async function navigateToNextCase() {
   const currentId = parseInt(route.params.id);
-  const totalCases = await fetchTotalCases(); // Angenommen, fetchTotalCases() ist eine Methode, die die Gesamtzahl der Fälle zurückgibt
+  const totalCases = await fetchTotalCases();
   const nextId = currentId + 1;
 
   if (nextId <= totalCases) {
@@ -282,28 +249,27 @@ async function navigateToNextCase() {
 async function fetchTotalCases() {
   try {
     const response = await axios.get('http://localhost:8081/api/email_threads');
-    return response.data.length; // Die Gesamtzahl der Fälle entspricht der Länge des zurückgegebenen Arrays
+    return response.data.length;
   } catch (error) {
     console.error('Error fetching total number of cases:', error);
-    return 0; // Falls ein Fehler auftritt, gehen wir von 0 Fällen aus
+    return 0;
   }
 }
 
 function saveFeaturesServerSide() {
-  const api_key = localStorage.getItem('api_key'); // API Key aus dem localStorage beziehen
+  const api_key = localStorage.getItem('api_key');
   if (!api_key) {
     alert('API key is missing');
     return;
   }
 
-  // Load the saved feature order from LocalStorage
   const savedFeatureOrder = localStorage.getItem(`featureOrder_${route.params.id}`);
   let orderedFeatures = groupedFeatures.value.map(group => ({
     type: group.type,
     details: group.details.map((detail, index) => ({
       model_name: detail.model_name,
       value: detail.value,
-      position: index // include position
+      position: index
     }))
   }));
 
@@ -326,11 +292,6 @@ function saveFeaturesServerSide() {
       alert('Fehler beim Speichern des Rankings.');
     });
 }
-
-function navigateToRanker() {
-  router.push({ name: 'Ranker' });
-}
-
 </script>
 
 <style scoped>
@@ -346,18 +307,18 @@ function navigateToRanker() {
 .email-thread-container {
   max-height: 500px;
   overflow-y: auto;
-  min-height: 80vh; /* Stellt sicher, dass der Container den gesamten Viewport einnimmt */
-  display: flex; /* Ermöglicht die Verwendung von Flexbox-Layout */
-  flex-direction: column; /* Orientiert die Kinder (Zeilen) vertikal */
-  position: relative; /* Wichtig für die Positionierung der Pseudo-Elemente */
+  min-height: 80vh;
+  display: flex;
+  flex-direction: column;
+  position: relative;
 }
 
 .fade-overlay {
   position: absolute;
   left: 0;
   right: 0;
-  height: 5px; /* Höhe des Überblendeffekts */
-  pointer-events: none; /* Verhindert, dass die Überlagerung Mausereignisse blockiert */
+  height: 5px;
+  pointer-events: none;
 }
 
 .fade-overlay.top {
@@ -382,15 +343,11 @@ function navigateToRanker() {
 }
 
 .same-sender {
-  background-color: #B2EBF2; /* Farbe für Nachrichten vom selben Sender */
+  background-color: #B2EBF2;
 }
 
 .different-sender {
-  background-color: #C8E6C9; /* Farbe für Nachrichten von verschiedenen Sendern */
-}
-
-.another-sender {
-  background-color: #F0F4C3; /* Eine weitere Farbe für die Unterscheidung */
+  background-color: #C8E6C9;
 }
 
 .message-header {
@@ -413,52 +370,35 @@ function navigateToRanker() {
 }
 
 .draggable-item {
-  background-color: #F0F4C3; /* Farbe */
-  border-radius: 33px 12px; /* unterschiedliche Radien */
-  padding: 15px; /* Innerer Abstand */
-  margin-bottom: 8px; /* Abstand unten */
-  cursor: grab; /* Cursor ändert sich zu einer Hand */
+  background-color: #F0F4C3;
+  border-radius: 33px 12px;
+  padding: 15px;
+  margin-bottom: 8px;
+  cursor: grab;
 }
 
 .draggable-item:active {
-  cursor: grabbing !important; /* Cursor ändert sich beim Greifen */
+  cursor: grabbing !important;
 }
 
 .fallbackStyleClass {
-  background-color: #528dc6; /* Farbe */
-  border-radius: 33px 12px; /* unterschiedliche Radien */
-  padding: 15px; /* Innerer Abstand */
-  margin-bottom: 8px; /* Abstand unten */
-  transform: rotate(1deg); /* Element wird leicht gedreht */
+  background-color: #528dc6;
+  border-radius: 33px 12px;
+  padding: 15px;
+  margin-bottom: 8px;
+  transform: rotate(1deg);
 }
 
 .no-select {
-  user-select: none; /* Verhindert die Textauswahl */
-}
-
-.fill-height {
-  height: 20vh; /* Höhe des Viewports */
-  flex-direction: column;
-  overflow: hidden; /* Verhindert das Scrollen des gesamten Layouts */
-}
-
-.row-height {
-  height: 100vh; /* oder jede andere Höhe */
-}
-
-.list-enter-active, .list-leave-active {
-  transition: all 0.5s ease;
-}
-.list-enter, .list-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
-}
-.ghost {
-  opacity: 0.0;
-  background: #c8ebfb;
+  user-select: none;
 }
 
 body.dragging * {
   cursor: grabbing !important;
+}
+
+.ghost {
+  opacity: 0.1;
+  background: #c8ebfb;
 }
 </style>

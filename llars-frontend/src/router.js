e5c5038e51e13065260f4874620c33af30bb4651
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-05-31 12:26:04 +0200

    add RaterDetail and RaterDetailFeature

--- llars-frontend/src/router.js
+++ llars-frontend/src/router.js
@@ -5,12 +5,16 @@ import NotFound from "@/components/NotFound.vue";
 import Ranker from "@/components/Ranker/Ranker.vue";
 import RankerDetail from "@/components/Ranker/RankerDetail.vue"; // Stellen Sie sicher, dass die Komponente existiert
 import Rater from "@/components/Rater/Rater.vue";
+import RaterDetail from "@/components/Rater/RaterDetail.vue";
+import RaterDetailFeature from "@/components/Rater/RaterDetailFeature.vue";
 
 const routes = [
     { path: '/Home', component: Home, meta: { requiresAuth: true } },
     { path: '/Ranker', component: Ranker, meta: { requiresAuth: true } },
     { path: '/Ranker/:id', name:'RankerDetail', component: RankerDetail, props: true, meta: { requiresAuth: true } }, // Detailroute für den Ranker
     { path: '/Rater', component: Rater, meta: { requiresAuth: true } },
+    { path: '/Rater/:id', name:'RaterDetail', component: RaterDetail, props: true, meta: { requiresAuth: true } }, // Detailroute für den Rater
+    { path: '/Rater/:id/:feature', name:'RaterDetailFeature', component: RaterDetailFeature, props: true, meta: { requiresAuth: true } }, // Detailroute für den Rater
     { path: '/login', component: Login, meta: { requiresAuth: false } },
 
     { path: '/', redirect: '/login' },

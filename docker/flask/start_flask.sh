2b4e443cb6aaeb58adabca1cc553a17f051ecd5b
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-09-24 15:14:36 +0200

    fix typo

--- docker/flask/start_flask.sh
+++ docker/flask/start_flask.sh
@@ -2,8 +2,8 @@
 
 # echo "Starting script in the background"
 # python your_script.py &
-echo "Waiting for 3 seconds before starting the Flask app..."
-sleep 5
+echo "Waiting for 2 seconds before starting the Flask app..."
+sleep 2
 
 # export FLASK_APP=main.py
 flask run --host=0.0.0.0 --port=8081

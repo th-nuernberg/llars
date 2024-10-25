dd428c4758267f350fae8d00c6b295b9c100dd40
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2024-10-25 11:04:28 +0200

    nginx conf

--- docker/supervisor/start_supervisor.sh
+++ docker/supervisor/start_supervisor.sh
@@ -3,7 +3,7 @@ echo "Starting supervisor..."
 echo "Waiting for Flask server to start..."
 # echo "Checking if Flask server is healthy..."
 # Wiederhole den Gesundheitscheck, bis der Server antwortet
-while ! curl -s http://backend-flask-service:8081/health_check | grep -q "Server is running"
+while ! curl -s http://backend-flask-service:8081/auth/health_check | grep -q "Server is running"
 do
   # echo "Waiting for the Flask server to become healthy..."
   # echo "Waiting for the Flask server to become healthy..."

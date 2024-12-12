7bba4f247c0e39a3a3cd7e0642baebe544e9134f
franzma84803 <franzma84803@th-nuernberg.de>
2024-12-12 10:46:15 +0100

    add generated by field to messages with default Value 'Human' set up structure to split routes into multiple files to organize stuff. Add Tables to create Rating Scenarios

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from db.db import configure_database
-from routes import configure_routes
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from routes_socketio import configure_socket_routes
-
+from routes import auth_blueprint, data_blueprint
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'super-geheimer-key'
 CORS(app)
@@ -13,8 +12,15 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 jwt = JWTManager(app)
 configure_database(app)
-configure_routes(app)
+
+# configure routes
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
+app.register_blueprint(data_blueprint, url_prefix='/api')
+
+
 configure_socket_routes(socketio)
+
+
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8081, debug=True)

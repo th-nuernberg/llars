0b4cbf37c91e02770a2bc385998023ad10454f93
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2025-11-28 18:23:37 +0100

    refactor(AdminRAG): Add composables index for clean exports
    Re-export all AdminRAG composables from single entry point:
    - useAdminRAGState
    - useAdminRAGActions
    - useAdminRAGHelpers

---
+++ llars-frontend/src/components/Admin/AdminRAG/index.js
@@ -0,0 +1,9 @@
+/**
+ * AdminRAG Composables Index
+ *
+ * Re-exports all composables for easy importing.
+ */
+
+export { useAdminRAGState } from './useAdminRAGState';
+export { useAdminRAGActions } from './useAdminRAGActions';
+export { useAdminRAGHelpers } from './useAdminRAGHelpers';

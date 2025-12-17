2c2c492b7852c73a529cb121cbffe82f8d139f01
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2025-12-17 19:01:51 +0100

    fix(frontend): add missing LCard component

--- llars-frontend/src/main.js
+++ llars-frontend/src/main.js
@@ -19,6 +19,7 @@ import App from './App.vue'
 import LBtn from '@/components/common/LBtn.vue'
 import LIconBtn from '@/components/common/LIconBtn.vue'
 import LTag from '@/components/common/LTag.vue'
+import LCard from '@/components/common/LCard.vue'
 import { initMatomo } from '@/plugins/llars-metrics'
 import { useAuth } from '@/composables/useAuth'
 
@@ -34,6 +35,7 @@ registerPlugins(app)
 app.component('LBtn', LBtn)
 app.component('LIconBtn', LIconBtn)
 app.component('LTag', LTag)
+app.component('LCard', LCard)
 
 // Set default Axios headers
 axios.defaults.headers.common['Content-Type'] = 'application/json'

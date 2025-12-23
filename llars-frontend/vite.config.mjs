47102666a9a74b1df835cb78b66f221f805b700e
steigerwald-ext <philipp.steigerwald@th-nuernberg.de>
2025-12-23 15:48:55 +0100

    feat(frontend): use local Roboto font instead of Google Fonts
    - Add local Roboto font files to avoid external dependencies
    - Remove ViteFonts plugin and Google Fonts integration
    - Use CSS @import for font loading

--- llars-frontend/vite.config.mjs
+++ llars-frontend/vite.config.mjs
@@ -2,7 +2,6 @@
 import Components from 'unplugin-vue-components/vite'
 import Vue from '@vitejs/plugin-vue'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ViteFonts from 'unplugin-fonts/vite'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -17,14 +16,6 @@ export default defineConfig({
     // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
     Vuetify({ autoImport: true }),
     Components(),
-    ViteFonts({
-      google: {
-        families: [{
-          name: 'Roboto',
-          styles: 'wght@100;300;400;500;700;900',
-        }],
-      },
-    }),
   ],
   define: { 'process.env': {} },
   resolve: {
